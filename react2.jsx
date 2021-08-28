9f3af96afd72855ff2885075fb670edb2ea1b417
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 16:21:23 +0530

    render html elements
    JSX program to render html elements in DOM.

---
+++ react2.jsx
@@ -0,0 +1,8 @@
+//JSX program to render a JSX element.
+const JSX = (
+    <div id="challenge-node">
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+);
+ReactDOM(JSX, document.getElementById("challenge-node"));
