154a6e5802eff92352827e21137662d3df12d22d
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 23:24:54 +0530

    Render a Class Component to the DOM
    JSX program to Render a Class Component to the DOM

---
+++ react9.jsx
@@ -0,0 +1,15 @@
+class TypesOfFood extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+          <Fruits></Fruits>
+          <Vegetables></Vegetables>
+        </div>
+      );
+    }
+  };
+ReactDOM.render(<TypesOfFood/>, document.getElementById("challenge-node"))
