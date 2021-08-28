583b070ed70dab467632602cd6731e1787ce5d1f
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 16:19:05 +0530

    JSX element
    JSX program to create a simple element.

---
+++ react1.jsx
@@ -0,0 +1,10 @@
+//JSX program to create a simple JSX element.
+const JSX = <div>
+    <h1>This is a program</h1>
+    <p></p>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
