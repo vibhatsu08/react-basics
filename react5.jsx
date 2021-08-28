c4663edac26043daf983696e145bcc712dabf8b3
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 23:00:17 +0530

    react component
    JSX program to create a react component

---
+++ react5.jsx
@@ -0,0 +1,13 @@
+//JSX program to create a react component
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return(
+        <div>
+          <h1>Hello React!</h1>
+      </div>  
+      );
+    }
+  };
