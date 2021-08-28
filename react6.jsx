a8d65869125754761d664985830e35c82a28e2b0
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 23:07:16 +0530

    create a component with composition
    JSX program to create a component with composition

---
+++ react6.jsx
@@ -0,0 +1,22 @@
+//create a component with composition
+const ChildComponent = () => {
+    return (
+      <div>
+        <p>I am the child</p>
+      </div>
+    );
+  };
+  
+  class ParentComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>I am the parent</h1>
+          <ChildComponent></ChildComponent>
+        </div>
+      );
+    }
+  };
