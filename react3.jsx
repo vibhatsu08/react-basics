7fabf20ec410d68db929053b285109b85c4daa3c
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 16:36:26 +0530

    JSX classes, comments, self closing tags
    JSX program to explain the new classes, comments and about self closing jsx tags

---
+++ react3.jsx
@@ -0,0 +1,18 @@
+//JSX program to explain the new classes, comments and about self closing jsx tags.
+//this is a comment in JSX.
+{/*This the way to create comments in JSX*/}
+const JSX = (
+    <div className = "challenge-node"> {/*This is the new convention of naming classes in JSX. In camelCase*/}
+     <h1>
+         <br/> {/*br tags in JSX, have closing tags, unlike how they were in HTML.*/}
+        <p></p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <hr/> {/*So does the hr tag in JSX. It must have a closing tag, the forward backslash.*/}
+    </h1>
+</div>
+);
+
